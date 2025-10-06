ed2608e6c5a0d839a9bff991e18aee7aaeed765c
felip-araujo <felipearaujoexecutive@gmail.com>
2025-10-06 10:04:22 -0400

    email variables

--- src/services/emailService.js
+++ src/services/emailService.js
@@ -3,8 +3,8 @@ import nodemailer from "nodemailer";
 export const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
-    user: "felipedgart@gmail.com",
-    pass: "effw yhbr byja xcbf",
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
 
