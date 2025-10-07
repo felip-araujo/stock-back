b0f823c8db0aa87b75179e2304857f78d6aa1d70
felip-araujo <felipearaujoexecutive@gmail.com>
2025-10-07 08:41:16 -0400

    correcting

--- src/controllers/statsDashCompany.js
+++ src/controllers/statsDashCompany.js
@@ -6,7 +6,7 @@ export const dashStats = async (req, res) => {
 
     const totalUsers = await prisma.user.count({ where: { companyId } });
     const totalProducts = await prisma.product.count({ where: { companyId } });
-    const totalMaterial = prisma.material.count({where: {companyId}})
+    const totalMaterial = await prisma.material.count({where: {companyId}})
 
     res.status(200).json({
       totalUsers,
